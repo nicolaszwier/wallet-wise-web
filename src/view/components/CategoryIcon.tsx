61263d49c180107a7d2cb370d051ce96cadc1ad5
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2025-01-29 21:11:25 -0500

    feat: changes to category icon component

--- src/view/components/CategoryIcon.tsx
+++ src/view/components/CategoryIcon.tsx
@@ -1,14 +1,15 @@
 import { CircleDollarSign } from "lucide-react"
 
 interface CategoryIconProps {
-  icon: string
+  icon?: string,
+  size?: number
 }
 
-export function CategoryIcon({icon}: CategoryIconProps) {
+export function CategoryIcon({icon, size}: CategoryIconProps) {
 
   return (
     <div className="rounded-lg bg-slate-300 p-1.5">
-      <CircleDollarSign size={22} />
+      <CircleDollarSign size={size ?? 22} />
     </div>
   )
 }
