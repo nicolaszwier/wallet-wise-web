e1b2406eedff74d48abccc29f122cbdc7713f7a7
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2025-04-04 13:07:37 -0300

    feat: added new api calls to the service

--- src/services/transactionsService/index.ts
+++ src/services/transactionsService/index.ts
@@ -5,11 +5,21 @@ import { Balance } from "@/app/models/Balance";
 
 export const transactionsService = {
   fetchDueThisWeek,
-  fetchMonthlyBalance
+  fetchMonthlyBalance,
+  create,
+  update,
+  pay
 };
 
 type TransactionsResponse = Transaction[];
-type BalanceResponse = Balance
+type BalanceResponse = Balance;
+type TransactionPayload = Transaction;
+
+interface DefaultResponse {
+  statusCode: number;
+  message: string;
+  error?: string;
+}
 
 async function fetchDueThisWeek(planningId: string) {
   const { data } = await httpClient.get<TransactionsResponse>(`/transactions/due-this-week/${planningId}`,);
@@ -21,5 +31,20 @@ async function fetchMonthlyBalance(planningId: string, month?: string, year?: st
     month, year
   }
   const { data } = await httpClient.get<BalanceResponse>(`/transactions/monthly_balance/${planningId}`, {params});
+  return data;
+}
+
+export async function create(payload: TransactionPayload) {
+  const { data } = await httpClient.post<DefaultResponse>('/transactions', payload);
+  return data;
+}
+
+export async function update(payload: TransactionPayload) {
+  const { data } = await httpClient.put<DefaultResponse>(`/transactions/${payload.id}`, payload);
+  return data;
+}
+
+export async function pay(periodId: string, transactionId: string) {
+  const { data } = await httpClient.put<DefaultResponse>(`/transactions/pay/${periodId}/${transactionId}`);
   return data;
 }
