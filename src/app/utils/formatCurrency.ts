874ebbbe1f7bc6fd0953ee54e26a933ec7888e75
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2025-04-04 13:06:38 -0300

    feat: new function to get the currency symbol

--- src/app/utils/formatCurrency.ts
+++ src/app/utils/formatCurrency.ts
@@ -4,3 +4,15 @@ export function formatCurrency(value: number, currency: string, locale: string) 
     currency: currency,
   }).format(value);
 }
+
+export function getCurrencySymbol (locale: string, currency: string) {
+  return (0).toLocaleString(
+    locale,
+    {
+      style: 'currency',
+      currency: currency,
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+    }
+  ).replace(/\d/g, '').trim()
+}
